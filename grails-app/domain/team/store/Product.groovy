934a6f64609611676ae7bcd568e4496526374d73
Emmett Looman <elooman@schillingschool.org>
2018-01-30 17:20:16 -0500

    Adding Working Product Domain class
    IntelliJ is finally working, added working Domain class.

--- grails-app/domain/team/store/Product.groovy
+++ grails-app/domain/team/store/Product.groovy
@@ -1,7 +1,17 @@
 package team.store
 
 class Product {
-
+    String name
+    String description
+    Double price
+    Category category
+    Manufacturer manufacturer
     static constraints = {
+        name nulable: false, blank:false
+        description nullable: false, blank: false
+        price nullable: false
+        category nullable:false
+        manufacturer nullable: false
+
     }
 }
