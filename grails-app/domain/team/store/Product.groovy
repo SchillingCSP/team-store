85b64d31d5ae3acd505f309a3adef28c010db0f1
Vivamun <4williamblack@gmail.com>
2018-02-06 09:36:17 -0500

    Revert "Adding Working Product Domain class"
    This reverts commit 934a6f64609611676ae7bcd568e4496526374d73.

--- grails-app/domain/team/store/Product.groovy
+++ grails-app/domain/team/store/Product.groovy
@@ -1,17 +1,7 @@
 package team.store
 
 class Product {
-    String name
-    String description
-    Double price
-    Category category
-    Manufacturer manufacturer
-    static constraints = {
-        name nulable: false, blank:false
-        description nullable: false, blank: false
-        price nullable: false
-        category nullable:false
-        manufacturer nullable: false
 
+    static constraints = {
     }
 }
