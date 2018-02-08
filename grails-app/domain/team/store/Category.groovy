0310a617c945f51621a915d15b48dcba1d096b9d
fianial <alexfiani@yahoo.com>
2018-02-08 09:28:04 -0500

    Category Domain Again

---
+++ grails-app/domain/team/store/Category.groovy
@@ -0,0 +1,13 @@
+ package team.store
+
+ class Category {
+     String name
+     static constraints = {
+         name nullable: false , blank: false
+     }
+     String toString(){
+         name
+     }
+
+
+ }
