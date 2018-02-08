dcd6ff7238fc835946dbd1e8ae7aecf425c84362
SeanSwayze <seanswayze1@gmail.com>
2018-02-08 09:38:36 -0500

    Revert "Merge branch 'Alex-Deryk' into Parker-Sean"
    This reverts commit b7c6a65d95f53120c04d091e2848bcb8965ca606, reversing
    changes made to 4c599163075c6336efcd230401ccf440d98d0147.

--- grails-app/domain/team/store/Category.groovy
+++ grails-app/domain/team/store/Category.groovy
@@ -1,9 +1,5 @@
 package team.store
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> Alex-Deryk
 class Category {
     String name
     static constraints = {
@@ -12,25 +8,6 @@ class Category {
     String toString(){
         name
     }
-<<<<<<< HEAD
 
 
 }
-=======
-
-
-}
-=======
- class Category {
-     String name
-     static constraints = {
-         name nullable: false , blank: false
-     }
-     String toString(){
-         name
-     }
-
-
- }
->>>>>>> 0310a617c945f51621a915d15b48dcba1d096b9d
->>>>>>> Alex-Deryk
