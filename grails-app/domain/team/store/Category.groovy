9aacef8c48268eb9d1090631f3f80174f654e28a
fianial <alexfiani@yahoo.com>
2018-02-13 08:54:59 -0500

    Category Domain and Category Views

--- grails-app/domain/team/store/Category.groovy
+++ grails-app/domain/team/store/Category.groovy
@@ -1,6 +1,5 @@
- package team.store
+package team.store
 
-<<<<<<< HEAD
 class Category {
     String name
     static constraints = {
@@ -12,16 +11,3 @@ class Category {
 
 
 }
-=======
- class Category {
-     String name
-     static constraints = {
-         name nullable: false , blank: false
-     }
-     String toString(){
-         name
-     }
-
-
- }
->>>>>>> 0310a617c945f51621a915d15b48dcba1d096b9d
